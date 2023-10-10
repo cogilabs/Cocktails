56ccb44fa2a864f2b71ebe3b13f0a2bdf5107458
David Girou <david.girou@0Davgi0.com>
2023-10-10 17:58:59 +0200

    props chapter

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,48 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Food</h1>
+  <p>My favorite food has a diploma image attached to it.</p>
+  <div id="wrapper">
+    <food-item
+      food-name="Apples"
+      food-desc="Apples are a type of fruit that grow on trees."
+      v-bind:is-favorite="true"/>
+    <food-item
+      food-name="Pizza"
+      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."
+      v-bind:is-favorite="false"/>
+    <food-item
+      food-name="Rice"
+      food-desc="Rice is a type of grain that people like to eat."
+      v-bind:is-favorite="false"/>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+  export default {
+    data() {
+      return {
+        message: 'This is some text'
+      };
+    }
+  };
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
+  #wrapper {
     display: flex;
-    place-items: flex-start;
     flex-wrap: wrap;
   }
-}
+  #wrapper > div {
+    border: dashed black 1px;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
 </style>
---
+++ src/components/FoodItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-on:click="countClicks(+1)"
+        v-on:click.right.prevent="countClicks(-1)">
+    <h2>
+      {{ foodName }}
+      <img src="/img_quality.svg" v-show="isFavorite">
+    </h2>
+    <p>{{ foodDesc }}</p>  
+    <p id="red">{{ clicks }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [
+      'foodName',
+      'foodDesc'
+    ],
+    data() {
+      return {
+        clicks: 0
+      }
+    },
+    methods: {
+      countClicks(val) {
+        this.clicks+=val;
+      }
+    }
+  }
+</script>
+
+<style>
+  #red {
+    font-weight: bold ;
+    color: rgb(144, 12, 12);
+  }
+</style>
