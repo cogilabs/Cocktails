5ca518a2bbaec49acdaf97153794873bbf50b0c7
0Davgi0 <0Davgi0@0DAVGI0-PC>
2023-10-10 23:07:35 +0200

    End of Vue v-for Components chapter

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,47 @@
 <template>
   <h1>Food</h1>
-  <p>My favorite food has a diploma image attached to it.</p>
+  <p>Food items are generated with v-for from the 'foods' array.</p>
   <div id="wrapper">
     <food-item
-      food-name="Apples"
-      food-desc="Apples are a type of fruit that grow on trees."
-      v-bind:is-favorite="false"/>
-    <food-item
-      food-name="Pizza"
-      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."
-      v-bind:is-favorite="true"/>
-    <food-item
-      food-name="Rice"
-      food-desc="Rice is a type of grain that people like to eat."
-      v-bind:is-favorite="false"/>
+      v-for="x in foods"
+      :key="x.name"
+      :food-name="x.name"
+      :food-desc="x.desc"
+      :is-favorite="x.favorite"/>
   </div>
 </template>
 
 <script>
+  export default {
+    data() {
+      return {
+        foods: [
+          { name: 'Apples',
+            desc: 'Apples are a type of fruit that grow on trees.',
+            favorite: true },
+          { name: 'Pizza',
+            desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.',
+            favorite: false },
+          { name: 'Rice',
+            desc: 'Rice is a type of grain that people like to eat.',
+            favorite: false },
+          { name: 'Fish',
+            desc: 'Fish is an animal that lives in water.',
+            favorite: true },
+          { name: 'Cake',
+            desc: 'Cake is something sweet that tastes good.',
+            favorite: false }
+        ]
+      };
+    }
+  }
 </script>
 
 <style>
   body {
     background-color: #202124;
     color: aliceblue;
+    font-family: Helvetica, Arial, sans-serif;
   }
   #wrapper {
     display: flex;
@@ -31,12 +49,10 @@
   }
   #wrapper > div {
     border: solid #494c50 1px;
+    color: #9aa0a6;
     flex-basis: 120px;
     margin: 10px;
     padding: 10px;
     background-color: #292a2d;
   }
-  #app > div:hover {
-  cursor: pointer;
-}
 </style>
