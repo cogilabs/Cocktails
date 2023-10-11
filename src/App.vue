2896de082a2de9834c7b72e5d6997a6c1e7f4aed
David Girou <david.girou@0Davgi0.com>
2023-10-11 14:50:59 +0200

    end of Vue HTTP Request chapter (silly robots)

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,31 @@
 <template>
-  <h3>Todo List</h3>  
-  <ul>
-    <todo-item
-      v-for="x in items"
-      :key="x"
-      :item-name="x"
-      style="background-color: #292a2d;"
-    />
-  </ul>
-  <input v-model="newItem">
-  <button @click="addItem">Add</button>
+  <h1>Example</h1>
+  <p>Click the button to fetch data with an HTTP request.</p>
+  <p>Each click generates an object with a random user from <a href="https://random-data-api.com/" target="_blank">https://random-data-api.com/</a>.</p>
+  <p>The robot avatars are lovingly delivered by <a href="http://Robohash.org" target="_blank">RoboHash</a>.</p>
+  <button @click="fetchData">Fetch data</button>
+  <div v-if="data" id="dataDiv">
+    <img :src="data.avatar" alt="avatar">
+    <pre>{{ data.first_name + " " + data.last_name }}</pre>
+    <p>"{{ data.employment.title }}"</p>
+  </div>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        newItem: '',
-        items: ['Buy apples','Make pizza','Mow the lawn']
+        data: null,
       };
     },
     methods: {
-      addItem() {
-        this.items.push(this.newItem),
-        this.newItem = '';
-      }
+      async fetchData() {      
+        const response = await fetch("https://random-data-api.com/api/v2/users"); 
+        this.data = await response.json();
+      },    
     }
-  }
-</script>
+  };
+</script>     
 
 <style>
   body {
@@ -35,16 +33,15 @@
     color: aliceblue;
     font-family: Helvetica, Arial, sans-serif;
   }
-  #wrapper {
-    display: flex;
-    flex-wrap: wrap;
-  }
-  #wrapper > div {
+  #dataDiv {
     border: solid #494c50 1px;
     color: #9aa0a6;
-    flex-basis: 120px;
-    margin: 10px;
+    width: 240px;
     padding: 10px;
+    margin-top: 10px;
     background-color: #292a2d;
+  }
+  #dataDiv > img {
+    width: 100%;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import TodoItem from './components/TodoItem.vue'
 
 const app = createApp(App)
-app.component('todo-item', TodoItem)
 app.mount('#app')
