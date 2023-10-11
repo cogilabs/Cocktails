5d265c0956ed173f089a3fc322ce6d22d32e90cd
David Girou <david.girou@0Davgi0.com>
2023-10-11 17:41:34 +0200

    end of Vue Routing chapter

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <template>
-  <h1>Example</h1>
-  <p>Click the button to fetch data with an HTTP request.</p>
-  <p>Each click generates an object with a random user from <a href="https://random-data-api.com/" target="_blank">https://random-data-api.com/</a>.</p>
-  <p>The robot avatars are lovingly delivered by <a href="http://Robohash.org" target="_blank">RoboHash</a>.</p>
-  <button @click="fetchData">Fetch data</button>
-  <div v-if="data" id="dataDiv">
-    <img :src="data.avatar" alt="avatar">
-    <pre>{{ data.first_name + " " + data.last_name }}</pre>
-    <p>"{{ data.employment.title }}"</p>
+  <p>Choose what part of this page you want to see:</p>
+  <router-link to="/animals">Animals</router-link>
+  <router-link to="/food">Food</router-link><br>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        data: null,
-      };
-    },
-    methods: {
-      async fetchData() {      
-        const response = await fetch("https://random-data-api.com/api/v2/users"); 
-        this.data = await response.json();
-      },    
-    }
-  };
-</script>     
+<style scoped>
+  a {
+    display: inline-block;
+    background-color: black;
+    border: solid 1px black;
+    color: white;
+    padding: 5px;
+    margin: 10px;
+  }
+  a:hover,
+  a.router-link-active {
+    background-color: rgb(110, 79, 13);
+  }
+  div {
+    border: dashed white 1px;
+    padding: 20px;
+    margin: 10px;
+    display: inline-block;
+  }
+</style>            
 
 <style>
   body {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
 import App from './App.vue'
+import FoodItems from './components/FoodItems.vue'
+import AnimalCollection from './components/AnimalCollection.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/animals', component: AnimalCollection },
+        { path: '/food', component: FoodItems },
+    ]
+});
 
 const app = createApp(App)
+
+app.use(router);
+
 app.mount('#app')
+                  
