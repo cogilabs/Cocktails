0d8f6fa9d5100b111c0b0e191d88d61fdb57c9eb
David Girou <david.girou@0Davgi0.com>
2023-10-12 10:14:30 +0200

    Project beginning

--- src/components/FoodItems.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Food!</h1>
-</template>           
--- src/main.js
+++ src/main.js
@@ -1,21 +1,8 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 
 import App from './App.vue'
-import FoodItems from './components/FoodItems.vue'
-import AnimalCollection from './components/AnimalCollection.vue'
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/animals', component: AnimalCollection },
-        { path: '/food', component: FoodItems },
-    ]
-});
+import CocktailComp from './components/CocktailComp.vue'
 
 const app = createApp(App)
-
-app.use(router);
-
+app.component('cocktail-comp', CocktailComp)
 app.mount('#app')
-                  
