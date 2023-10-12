c1234a94fde1d56d1216ed53bacfc2e83cd274ff
David Girou <david.girou@0Davgi0.com>
2023-10-12 14:49:51 +0200

    Test actions

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,63 @@
 <template>
-  <p>Page in construction</p>
-</template>         
+    <cocktail-comp 
+      v-for="x in ings"
+      v-bind:ing-name="x"/>
+</template>   
+
+<script>
+  export default {
+    data() {
+      return {
+        data: null,
+        ings: []
+      };
+    },
+    mounted() {
+      this.fetchData();
+    },
+    methods: {
+      async fetchData() {
+        const response = await fetch("cocktails.json");
+        const json = await response.json();
+        const ingList = new Array();
+        for (const i in json) {
+          for (const j in json[i].Ingredients) {
+            if (ingList.length) {
+              let okToAdd = true;
+              for (const k in ingList) {
+                if (json[i].Ingredients[j] == ingList[k]) {
+                  okToAdd = false;
+                }
+              }
+              if (okToAdd) {
+                ingList.push(json[i].Ingredients[j]);
+              }
+            } else {
+              ingList.push(json[i].Ingredients[j]);
+            }            
+          }
+        }
+        this.ings = ingList;
+      }
+    }
+  }
+</script>      
 
 <style>
   body {
-    background-color: #202124;
-    color: aliceblue;
+    background-color: #aab3ce;
+    color: #494c50;
     font-family: Helvetica, Arial, sans-serif;
   }
-  #dataDiv {
+  .ing {
     border: solid #494c50 1px;
     color: #9aa0a6;
-    width: 240px;
+    width: 100px;
     padding: 10px;
     margin-top: 10px;
-    background-color: #292a2d;
-  }
-  #dataDiv > img {
-    width: 100%;
+    background-color: #e4ebff;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    border-radius: 10px;
+    margin: 10px;
   }
 </style>
--- src/components/CocktailComp.vue
+++ src/components/CocktailComp.vue
@@ -1,7 +1,11 @@
 <template>
-
+  <div class="ing">
+    <h2>{{ ingName }}</h2>
+  </div>
 </template>
 
 <script>
-
+export default {
+  props: ['ingName']
+}
 </script>
