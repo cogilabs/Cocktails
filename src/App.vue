c691c17c0a8206e081f3deb3e84d166c0a844a7f
David Girou <david.girou@0Davgi0.com>
2023-10-16 15:02:27 +0200

    Added Cogilabs dots&line background

--- src/App.vue
+++ src/App.vue
@@ -269,4 +269,13 @@
   .fade-move {
     transition: all 0.7s
   }
+
+  #dotsAndLinesCanvas {
+    position: fixed;
+    left:0;
+    top:0;
+    z-index:-1;
+    width: 100%;
+    height: 100%;
+  }
 </style>
