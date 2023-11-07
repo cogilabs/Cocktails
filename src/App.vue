3aa3e44300f9c320e2cabc70413310718f21adb1
David Girou <david.girou@0Davgi0.com>
2023-11-07 14:49:02 +0100

    Set the cogilabs link to a new window/tab

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <router-view></router-view>
   </div>
-  <div style="position: fixed; bottom: 20px; right: 20px"><a href="https://Cogilabs.eu/"><img src="https://Cogilabs.eu/Images/Cogilabs-long-contour-wh-notxt.svg" alt="The Cogilabs logo" height="50"></a></div>
+  <div style="position: fixed; bottom: 20px; right: 20px"><a target="_blank" href="https://Cogilabs.eu/"><img src="https://Cogilabs.eu/Images/Cogilabs-long-contour-wh-notxt.svg" alt="The Cogilabs logo" height="50"></a></div>
 </template>
 
 <style>
